c64e08e7525cf25e226f40957c3cbd0b6d5d0d46
LaurenJMKruse <laurenjmkruse@gmail.com>
2021-04-14 11:52:30 -0400

    Adding server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+// *******************************
+// A. Establish dependency
+const express = require('express');
+
+
+// *******************************
+// B. Connect routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+// *******************************
+// C. Initialize Express
+const app = express();
+
+
+// *******************************
+// D. Establish port
+const PORT = process.env.PORT || 3000;
+
+
+// *******************************
+// E. Establish Express middleware
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+// *******************************
+// F. Initialize server on the port
+app.listen(PORT, () => {
+    console.log(`Server now listening on PORT ${PORT}`);
+});
