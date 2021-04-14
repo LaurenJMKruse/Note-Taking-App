309e611b604a64c4963d5841740fe555504cba6e
LaurenJMKruse <laurenjmkruse@gmail.com>
2021-04-14 12:29:45 -0400

    Created apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,51 @@
+// *******************************
+// A. Connect dependency (Express)
+const router = require('express').Router();
+
+
+// *******************************
+// B. Connect archive.js
+const archive = require('../db/archive');
+
+
+// *******************************
+// C. CRUD Actions
+
+// 01. Retrieve all notes
+router.get('/notes', (req, res) => {
+    archive
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        }).catch((error) => {
+            console.log(`An error has occurred!`);
+            res.status(500).json(error);
+        });
+});
+
+// 02. Post a new note
+router.post('/notes', (req, res) => {
+    archive
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch((error) => {
+            console.log(`An error has occurred!`);
+            res.status(500).json(error);
+        });
+});
+
+// 03. Delete a note
+router.delete('/notes/:id', (req, res) => {
+    archive
+        .deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((error) => {
+            console.log(`An error has occurred!`);
+            res.status(500).json(error);
+        });
+});
+
+
+// *******************************
+// D. Export router
+module.exports = router;
